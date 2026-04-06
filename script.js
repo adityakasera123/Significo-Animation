cc568a06f17a29496a2eb833366d9a5ed0ddae06
Aditya <adityakasera40@gmail.com>
2026-04-06 15:13:20 +0530

    complete Text para part

--- script.js
+++ script.js
@@ -35,16 +35,97 @@ tl.to(".slidesm",{
 
 
 }
-homePageAnimation();
 
-gsap.to(".slide",{
+function realPageAnimation(){
+    gsap.to(".slide",{
     scrollTrigger:{
         trigger:".real",
         start:"top top",
         end:"bottom bottom",
-        markers:true,
+ 
         scrub:1
     },
     xPercent:-335,
     ease:Power4
 })
+}
+
+function teamAnimation(){
+    document.querySelectorAll(".listelem")
+.forEach(function(el){
+el.addEventListener("mousemove",function(dets){
+   
+    gsap.to(this.querySelector(".picture"), 
+        {opacity:1, 
+        x: gsap.utils.mapRange(0, window.innerWidth, -200,200, dets.clientX), 
+        ease:Power4,
+        duration:.5})
+})
+el.addEventListener("mouseleave", function(dets){
+    gsap.to(this.querySelector(".picture"), 
+         {opacity:0,
+         ease:Power4, 
+         duration:.5})
+})
+})
+}
+
+function paraAnimation(){
+    let clutter = "";
+
+document.querySelector(".textpara").textContent.split("").forEach(function(e) {
+    if (e === " ") {
+        clutter += `<span>&nbsp;</span>`;
+    } else {
+        clutter += `<span>${e}</span>`;
+    }
+});
+
+document.querySelector(".textpara").innerHTML = clutter;
+gsap.set(".textpara span", {opacity:.1})
+gsap.to(".textpara span", {
+    scrollTrigger:{
+        trigger:".para",
+        start:"top 70%",
+        end:"bottom 95%",
+
+        scrub:.2
+    },
+    opacity:1,
+    stagger:.03,
+    ease:Power4
+})
+
+let clutter1 = "";
+
+document.querySelector(".textpara1").textContent.split("").forEach(function(e) {
+    if (e === " ") {
+        clutter1 += `<span>&nbsp;</span>`;
+    } else {
+        clutter1 += `<span>${e}</span>`;
+    }
+});
+
+document.querySelector(".textpara1").innerHTML = clutter1;
+gsap.set(".textpara1 span", {opacity:.1})
+gsap.to(".textpara1 span", {
+    scrollTrigger:{
+        trigger:".para1",
+        start:"top 90%",
+        end:"bottom 95%",
+
+        scrub:.2
+    },
+    opacity:1,
+    stagger:.03,
+    ease:Power4
+})
+}
+
+
+homePageAnimation();
+realPageAnimation();
+teamAnimation();
+paraAnimation();
+
+
